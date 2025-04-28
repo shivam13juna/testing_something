0ff1cdd6e11b13ad2b4f06a23494a9841359c48f
SHIVAM PRASAD <shivam13juna@gmail.com>
2025-04-28 08:38:58 +0000

    Fixing a simple bug in the calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
-\
+
 def add(x, y):
-    \"\"\"Adds two numbers.\"\"\"
+    #"\"\"Adds two numbers.\"\"\"
     return x + y
 
 def subtract(x, y):
-    \"\"\"Subtracts two numbers.\"\"\"
+    #"\"\"Subtracts two numbers.\"\"\"
     return x - y
 
 def multiply(x, y):
-    \"\"\"Multiplies two numbers.\"\"\"
+    # "\"\"Multiplies two numbers.\"\"\"
     return x * y
 
 def divide(x, y):
-    \"\"\"Divides two numbers. Raises ValueError on division by zero.\"\"\"
+    #"\"\"Divides two numbers. Raises ValueError on division by zero.\"\"\"
     if y == 0:
         raise ValueError("Cannot divide by zero!")
     return x / y
